8f636675ba9becfa6665c66b735ce158d4fcae2d
Arkadiy <99073279+Deflnam@users.noreply.github.com>
2026-05-15 01:55:59 +0300

    List: add pushBack, erase, const begin/end for S3

--- burukov.arkadiy/common/list.hpp
+++ burukov.arkadiy/common/list.hpp
@@ -14,14 +14,8 @@ namespace burukov
   {
     T val_;
     Node< T > *next_;
-    Node(const T &value, Node< T > *next):
-      val_(value),
-      next_(next)
-    {}
-    Node(T &&value, Node< T > *next):
-      val_(std::move(value)),
-      next_(next)
-    {}
+    Node(const T &value, Node< T > *next) : val_(value), next_(next) {}
+    Node(T &&value, Node< T > *next) : val_(std::move(value)), next_(next) {}
   };
 
   template< class T >
@@ -29,40 +23,19 @@ namespace burukov
   {
     friend class List< T >;
   public:
-    LIter():
-      ptr_(nullptr)
-    {}
-    explicit LIter(Node< T > *p):
-      ptr_(p)
-    {}
-    T &operator*()
-    {
-      return ptr_->val_;
-    }
-    T *operator->()
-    {
-      return &(ptr_->val_);
-    }
-    LIter< T > &operator++()
-    {
-      ptr_ = ptr_->next_;
-      return *this;
-    }
+    LIter() : ptr_(nullptr) {}
+    explicit LIter(Node< T > *p) : ptr_(p) {}
+    T &operator*() { return ptr_->val_; }
+    T *operator->() { return &(ptr_->val_); }
+    LIter< T > &operator++() { ptr_ = ptr_->next_; return *this; }
     LIter< T > operator++(int)
     {
       LIter< T > temp(*this);
       ptr_ = ptr_->next_;
       return temp;
     }
-    bool operator==(const LIter< T > &rhs) const
-    {
-      return ptr_ == rhs.ptr_;
-    }
-    bool operator!=(const LIter< T > &rhs) const
-    {
-      return ptr_ != rhs.ptr_;
-    }
-
+    bool operator==(const LIter< T > &rhs) const { return ptr_ == rhs.ptr_; }
+    bool operator!=(const LIter< T > &rhs) const { return ptr_ != rhs.ptr_; }
   private:
     Node< T > *ptr_;
   };
@@ -72,40 +45,19 @@ namespace burukov
   {
     friend class List< T >;
   public:
-    LCIter():
-      ptr_(nullptr)
-    {}
-    explicit LCIter(const Node< T > *p):
-      ptr_(p)
-    {}
-    const T &operator*() const
-    {
-      return ptr_->val_;
-    }
-    const T *operator->() const
-    {
-      return &(ptr_->val_);
-    }
-    LCIter< T > &operator++()
-    {
-      ptr_ = ptr_->next_;
-      return *this;
-    }
+    LCIter() : ptr_(nullptr) {}
+    explicit LCIter(const Node< T > *p) : ptr_(p) {}
+    const T &operator*() const { return ptr_->val_; }
+    const T *operator->() const { return &(ptr_->val_); }
+    LCIter< T > &operator++() { ptr_ = ptr_->next_; return *this; }
     LCIter< T > operator++(int)
     {
       LCIter< T > temp(*this);
       ptr_ = ptr_->next_;
       return temp;
     }
-    bool operator==(const LCIter< T > &rhs) const
-    {
-      return ptr_ == rhs.ptr_;
-    }
-    bool operator!=(const LCIter< T > &rhs) const
-    {
-      return ptr_ != rhs.ptr_;
-    }
-
+    bool operator==(const LCIter< T > &rhs) const { return ptr_ == rhs.ptr_; }
+    bool operator!=(const LCIter< T > &rhs) const { return ptr_ != rhs.ptr_; }
   private:
     const Node< T > *ptr_;
   };
@@ -114,13 +66,9 @@ namespace burukov
   class List
   {
   public:
-    List():
-      head_(nullptr),
-      size_(0)
-    {}
-    List(const List< T > &other):
-      head_(nullptr),
-      size_(0)
+    List() : head_(nullptr), size_(0) {}
+
+    List(const List< T > &other) : head_(nullptr), size_(0)
     {
       Node< T > *src = other.head_;
       Node< T > *dst = nullptr;
@@ -137,29 +85,23 @@ namespace burukov
           throw;
         }
         if (!head_)
-        {
           head_ = created;
-        }
         else
-        {
           dst->next_ = created;
-        }
         dst = created;
         ++size_;
         src = src->next_;
       }
     }
-    List(List< T > &&other):
-      head_(other.head_),
-      size_(other.size_)
+
+    List(List< T > &&other) : head_(other.head_), size_(other.size_)
     {
       other.head_ = nullptr;
       other.size_ = 0;
     }
-    ~List()
-    {
-      clear();
-    }
+
+    ~List() { clear(); }
+
     List< T > &operator=(const List< T > &other)
     {
       if (this != &other)
@@ -169,6 +111,7 @@ namespace burukov
       }
       return *this;
     }
+
     List< T > &operator=(List< T > &&other)
     {
       if (this != &other)
@@ -181,32 +124,24 @@ namespace burukov
       }
       return *this;
     }
-    bool empty() const
-    {
-      return size_ == 0;
-    }
-    size_t size() const
-    {
-      return size_;
-    }
-    T &front()
-    {
-      return head_->val_;
-    }
-    const T &front() const
-    {
-      return head_->val_;
-    }
+
+    bool empty() const { return size_ == 0; }
+    size_t size() const { return size_; }
+    T &front() { return head_->val_; }
+    const T &front() const { return head_->val_; }
+
     void pushFront(const T &val)
     {
       head_ = new Node< T >(val, head_);
       ++size_;
     }
+
     void pushFront(T &&val)
     {
       head_ = new Node< T >(std::move(val), head_);
       ++size_;
     }
+
     void popFront()
     {
       Node< T > *old = head_;
@@ -214,6 +149,7 @@ namespace burukov
       delete old;
       --size_;
     }
+
     LIter< T > insertAfter(LIter< T > pos, const T &val)
     {
       Node< T > *created = new Node< T >(val, pos.ptr_->next_);
@@ -221,14 +157,15 @@ namespace burukov
       ++size_;
       return LIter< T >(created);
     }
+
     LIter< T > insertAfter(LIter< T > pos, T &&val)
     {
-      Node< T > *created =
-          new Node< T >(std::move(val), pos.ptr_->next_);
+      Node< T > *created = new Node< T >(std::move(val), pos.ptr_->next_);
       pos.ptr_->next_ = created;
       ++size_;
       return LIter< T >(created);
     }
+
     LIter< T > eraseAfter(LIter< T > pos)
     {
       Node< T > *victim = pos.ptr_->next_;
@@ -237,6 +174,7 @@ namespace burukov
       --size_;
       return LIter< T >(pos.ptr_->next_);
     }
+
     void clear()
     {
       while (head_)
@@ -247,31 +185,66 @@ namespace burukov
       }
       size_ = 0;
     }
+
     void swap(List< T > &other)
     {
-      Node< T > *tmpHead = head_;
-      head_ = other.head_;
-      other.head_ = tmpHead;
-      const size_t tmpSize = size_;
-      size_ = other.size_;
-      other.size_ = tmpSize;
+      std::swap(head_, other.head_);
+      std::swap(size_, other.size_);
     }
-    LIter< T > begin()
+
+    LIter< T > begin() { return LIter< T >(head_); }
+    LIter< T > end() { return LIter< T >(nullptr); }
+    LCIter< T > cbegin() const { return LCIter< T >(head_); }
+    LCIter< T > cend() const { return LCIter< T >(nullptr); }
+
+    void pushBack(const T &val)
     {
-      return LIter< T >(head_);
+      Node<T> *nn = new Node<T>(val, nullptr);
+      if (!head_)
+        head_ = nn;
+      else
+      {
+        Node<T> *cur = head_;
+        while (cur->next_)
+          cur = cur->next_;
+        cur->next_ = nn;
+      }
+      ++size_;
     }
-    LIter< T > end()
+
+    void pushBack(T &&val)
     {
-      return LIter< T >(nullptr);
+      Node<T> *nn = new Node<T>(std::move(val), nullptr);
+      if (!head_)
+        head_ = nn;
+      else
+      {
+        Node<T> *cur = head_;
+        while (cur->next_)
+          cur = cur->next_;
+        cur->next_ = nn;
+      }
+      ++size_;
     }
-    LCIter< T > cbegin() const
+
+    LIter<T> erase(LIter<T> pos)
     {
-      return LCIter< T >(head_);
+      if (pos.ptr_ == head_)
+      {
+        popFront();
+        return LIter<T>(head_);
+      }
+      Node<T> *prev = head_;
+      while (prev->next_ != pos.ptr_)
+        prev = prev->next_;
+      prev->next_ = pos.ptr_->next_;
+      delete pos.ptr_;
+      --size_;
+      return LIter<T>(prev->next_);
     }
-    LCIter< T > cend() const
-    {
-      return LCIter< T >(nullptr);
-    }
+
+    LCIter<T> begin() const { return cbegin(); }
+    LCIter<T> end() const { return cend(); }
 
   private:
     Node< T > *head_;
