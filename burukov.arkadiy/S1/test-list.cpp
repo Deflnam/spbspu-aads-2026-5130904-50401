ce011c8d96a820d43e8d30347f2bbcca6c787707
Arkadiy <99073279+Deflnam@users.noreply.github.com>
2026-05-19 22:22:48 +0300

    add additional assignment

--- burukov.arkadiy/S1/test-list.cpp
+++ burukov.arkadiy/S1/test-list.cpp
@@ -304,4 +304,266 @@ BOOST_AUTO_TEST_CASE(ArrowOperatorTest)
   BOOST_CHECK_EQUAL(it->second, 2);
 }
 
+BOOST_AUTO_TEST_CASE(SpliceWholeListTest)
+{
+  burukov::List< int > list1;
+  list1.pushFront(3);
+  list1.pushFront(2);
+  list1.pushFront(1);
+  burukov::List< int > list2;
+  list2.pushFront(6);
+  list2.pushFront(5);
+  list2.pushFront(4);
+  list1.splice(list1.begin(), list2);
+  BOOST_CHECK_EQUAL(list1.size(), 6);
+  BOOST_CHECK(list2.empty());
+  auto it = list1.begin();
+  BOOST_CHECK_EQUAL(*it, 4);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 5);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 6);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 1);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 2);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 3);
+}
+
+BOOST_AUTO_TEST_CASE(SpliceSingleElementTest)
+{
+  burukov::List< int > list1;
+  list1.pushFront(3);
+  list1.pushFront(1);
+  burukov::List< int > list2;
+  list2.pushFront(4);
+  list2.pushFront(2);
+  auto it = list1.begin();
+  ++it;
+  auto it2 = list2.begin();
+  ++it2;
+  list1.splice(it, list2, it2);
+  BOOST_CHECK_EQUAL(list1.size(), 3);
+  BOOST_CHECK_EQUAL(list2.size(), 1);
+  auto check = list1.begin();
+  BOOST_CHECK_EQUAL(*check, 1);
+  ++check;
+  BOOST_CHECK_EQUAL(*check, 4);
+  ++check;
+  BOOST_CHECK_EQUAL(*check, 3);
+}
+
+BOOST_AUTO_TEST_CASE(SpliceRangeTest)
+{
+  burukov::List< int > list1;
+  list1.pushFront(5);
+  list1.pushFront(1);
+  burukov::List< int > list2;
+  list2.pushFront(6);
+  list2.pushFront(4);
+  list2.pushFront(3);
+  list2.pushFront(2);
+  auto pos = list1.begin();
+  ++pos;
+  auto first = list2.begin();
+  ++first;
+  auto last = list2.end();
+  list1.splice(pos, list2, first, last);
+  BOOST_CHECK_EQUAL(list1.size(), 4);
+  auto it = list1.begin();
+  BOOST_CHECK_EQUAL(*it, 1);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 3);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 4);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 5);
+}
+
+BOOST_AUTO_TEST_CASE(SortTest)
+{
+  burukov::List< int > list;
+  list.pushFront(5);
+  list.pushFront(3);
+  list.pushFront(1);
+  list.pushFront(4);
+  list.pushFront(2);
+  list.sort();
+  auto it = list.begin();
+  BOOST_CHECK_EQUAL(*it, 1);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 2);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 3);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 4);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 5);
+}
+
+BOOST_AUTO_TEST_CASE(SortDescendingTest)
+{
+  burukov::List< int > list;
+  list.pushFront(1);
+  list.pushFront(2);
+  list.pushFront(3);
+  list.pushFront(4);
+  list.pushFront(5);
+  list.sort(std::greater< int >());
+  auto it = list.begin();
+  BOOST_CHECK_EQUAL(*it, 5);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 4);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 3);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 2);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 1);
+}
+
+BOOST_AUTO_TEST_CASE(SortEmptyTest)
+{
+  burukov::List< int > list;
+  list.sort();
+  BOOST_CHECK(list.empty());
+}
+
+BOOST_AUTO_TEST_CASE(SortSingleElementTest)
+{
+  burukov::List< int > list;
+  list.pushFront(42);
+  list.sort();
+  BOOST_CHECK_EQUAL(list.size(), 1);
+  BOOST_CHECK_EQUAL(list.front(), 42);
+}
+
+BOOST_AUTO_TEST_CASE(MergeTest)
+{
+  burukov::List< int > list1;
+  list1.pushFront(3);
+  list1.pushFront(1);
+  burukov::List< int > list2;
+  list2.pushFront(4);
+  list2.pushFront(2);
+  list1.merge(list2);
+  BOOST_CHECK_EQUAL(list1.size(), 4);
+  BOOST_CHECK(list2.empty());
+  auto it = list1.begin();
+  BOOST_CHECK_EQUAL(*it, 1);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 2);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 3);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 4);
+}
+
+BOOST_AUTO_TEST_CASE(MergeWithComparatorTest)
+{
+  burukov::List< int > list1;
+  list1.pushFront(1);
+  list1.pushFront(3);
+  burukov::List< int > list2;
+  list2.pushFront(2);
+  list2.pushFront(4);
+  list1.merge(list2, std::greater< int >());
+  auto it = list1.begin();
+  BOOST_CHECK_EQUAL(*it, 4);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 3);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 2);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 1);
+}
+
+BOOST_AUTO_TEST_CASE(MergeEmptyTest)
+{
+  burukov::List< int > list1;
+  list1.pushFront(1);
+  burukov::List< int > list2;
+  list1.merge(list2);
+  BOOST_CHECK_EQUAL(list1.size(), 1);
+  BOOST_CHECK(list2.empty());
+}
+
+BOOST_AUTO_TEST_CASE(PartitionTest)
+{
+  burukov::List< int > list;
+  list.pushFront(5);
+  list.pushFront(2);
+  list.pushFront(4);
+  list.pushFront(1);
+  list.pushFront(3);
+  auto isOdd = [](const int &x)
+  {
+    return x % 2 == 1;
+  };
+  list.partition(isOdd);
+  auto it = list.begin();
+  bool foundEven = false;
+  for (; it != list.end(); ++it)
+  {
+    if (*it % 2 == 0)
+    {
+      foundEven = true;
+    }
+    else if (foundEven)
+    {
+      BOOST_CHECK(false);
+    }
+  }
+}
+
+BOOST_AUTO_TEST_CASE(PartitionEmptyTest)
+{
+  burukov::List< int > list;
+  auto isOdd = [](const int &x)
+  {
+    return x % 2 == 1;
+  };
+  auto result = list.partition(isOdd);
+  BOOST_CHECK(result == list.end());
+}
+
+BOOST_AUTO_TEST_CASE(PartitionAllTrueTest)
+{
+  burukov::List< int > list;
+  list.pushFront(5);
+  list.pushFront(3);
+  list.pushFront(1);
+  auto isOdd = [](const int &x)
+  {
+    return x % 2 == 1;
+  };
+  list.partition(isOdd);
+  auto it = list.begin();
+  BOOST_CHECK_EQUAL(*it, 1);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 3);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 5);
+}
+
+BOOST_AUTO_TEST_CASE(PartitionAllFalseTest)
+{
+  burukov::List< int > list;
+  list.pushFront(4);
+  list.pushFront(2);
+  list.pushFront(6);
+  auto isOdd = [](const int &x)
+  {
+    return x % 2 == 1;
+  };
+  list.partition(isOdd);
+  auto it = list.begin();
+  BOOST_CHECK_EQUAL(*it, 6);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 2);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 4);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
