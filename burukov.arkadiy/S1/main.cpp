05c9013a2376dc0945f56def50a39e01b20e4e30
Arkadiy <99073279+Deflnam@users.noreply.github.com>
2026-05-25 11:17:44 +0300

    fix

--- burukov.arkadiy/S1/main.cpp
+++ burukov.arkadiy/S1/main.cpp
@@ -10,9 +10,7 @@ namespace burukov
   using pair_t = std::pair<std::string,List< unsigned long long >>;
 
   template< class Container >
-  void printContainer(
-      std::ostream &out,
-      const Container &container)
+  void printContainer(std::ostream &out,const Container &container)
   {
     auto it = container.cbegin();
 
@@ -28,9 +26,7 @@ namespace burukov
     }
   }
 
-  void printNames(
-      std::ostream &out,
-      const List< pair_t > &seqs)
+  void printNames(std::ostream &out,const List< pair_t > &seqs)
   {
     auto it = seqs.cbegin();
 
