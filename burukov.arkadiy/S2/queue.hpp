23df643a6f42d2e886ffc6d2c7de49dfeea591d3
Arkadiy <99073279+Deflnam@users.noreply.github.com>
2026-05-30 22:37:27 +0300

    Refactor queue interface

--- burukov.arkadiy/S2/queue.hpp
+++ burukov.arkadiy/S2/queue.hpp
@@ -9,123 +9,118 @@ namespace burukov
   class Queue
   {
   public:
-    Queue():
-      list_(),
-      tail_()
-    {}
+    Queue() = default;
 
-    Queue(const Queue &other):
-      list_(other.list_),
-      tail_()
-    {
-      findTail();
-    }
+    Queue(const Queue &other);
+    Queue(Queue &&other) noexcept;
 
-    Queue(Queue &&other):
-      list_(std::move(other.list_)),
-      tail_(other.tail_)
-    {
-      other.tail_ = LIter< T >();
-    }
+    Queue &operator=(const Queue &other);
+    Queue &operator=(Queue &&other) noexcept;
 
-    Queue &operator=(const Queue &other)
-    {
-      if (this != &other)
-      {
-        Queue temp(other);
-        swap(temp);
-      }
-      return *this;
-    }
+    T &front();
+    const T &front() const;
 
-    Queue &operator=(Queue &&other)
-    {
-      if (this != &other)
-      {
-        Queue temp(std::move(other));
-        swap(temp);
-      }
-      return *this;
-    }
+    bool empty() const noexcept;
+    size_t size() const noexcept;
 
-    T &front()
-    {
-      return list_.front();
-    }
+    void push(const T &value);
+    void push(T &&value);
 
-    const T &front() const
-    {
-      return list_.front();
-    }
+    void pop() noexcept;
 
-    bool empty() const
-    {
-      return list_.size() == 0;
-    }
+    void swap(Queue &other) noexcept;
 
-    size_t size() const
-    {
-      return list_.size();
-    }
-
-    void push(const T &value)
-    {
-      if (list_.empty())
-      {
-        list_.pushFront(value);
-        tail_ = list_.begin();
-      }
-      else
-      {
-        tail_ = list_.insertAfter(tail_, value);
-      }
-    }
-
-    void pop()
-    {
-      if (list_.size() == 1)
-      {
-        tail_ = LIter< T >();
-      }
-      list_.popFront();
-    }
-
-    void swap(Queue &other)
-    {
-      list_.swap(other.list_);
-      const LIter< T > tmp = tail_;
-      tail_ = other.tail_;
-      other.tail_ = tmp;
-    }
-
-    void clear()
-    {
-      list_.clear();
-      tail_ = LIter< T >();
-    }
+    void clear() noexcept;
 
   private:
     List< T > list_;
-    LIter< T > tail_;
-
-    void findTail()
-    {
-      tail_ = LIter< T >();
-      if (list_.empty())
-      {
-        return;
-      }
-      LIter< T > it = list_.begin();
-      LIter< T > next = it;
-      ++next;
-      while (next != list_.end())
-      {
-        it = next;
-        ++next;
-      }
-      tail_ = it;
-    }
   };
+}
+
+template< class T >
+burukov::Queue< T >::Queue(const Queue &other)
+  : list_(other.list_)
+{}
+
+template< class T >
+burukov::Queue< T >::Queue(Queue &&other) noexcept
+  : list_(std::move(other.list_))
+{}
+
+template< class T >
+burukov::Queue< T > &burukov::Queue< T >::operator=(const Queue &other)
+{
+  if (this != &other)
+  {
+    Queue temp(other);
+    swap(temp);
+  }
+  return *this;
+}
+
+template< class T >
+burukov::Queue< T > &burukov::Queue< T >::operator=(Queue &&other) noexcept
+{
+  if (this != &other)
+  {
+    Queue temp(std::move(other));
+    swap(temp);
+  }
+  return *this;
+}
+
+template< class T >
+T &burukov::Queue< T >::front()
+{
+  return list_.front();
+}
+
+template< class T >
+const T &burukov::Queue< T >::front() const
+{
+  return list_.front();
+}
+
+template< class T >
+bool burukov::Queue< T >::empty() const noexcept
+{
+  return list_.size() == 0;
+}
+
+template< class T >
+size_t burukov::Queue< T >::size() const noexcept
+{
+  return list_.size();
+}
+
+template< class T >
+void burukov::Queue< T >::push(const T &value)
+{
+  list_.pushBack(value);
+}
+
+template< class T >
+void burukov::Queue< T >::push(T &&value)
+{
+  list_.pushBack(std::move(value));
+}
+
+template< class T >
+void burukov::Queue< T >::pop() noexcept
+{
+  list_.popFront();
+}
+
+template< class T >
+void burukov::Queue< T >::swap(Queue &other) noexcept
+{
+  list_.swap(other.list_);
+}
+
+template< class T >
+void burukov::Queue< T >::clear() noexcept
+{
+  list_.clear();
 }
 
 #endif
