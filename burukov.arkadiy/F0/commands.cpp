5d4fef063354831c83304093f2c5f9231ae8fc33
Arkadiy <99073279+Deflnam@users.noreply.github.com>
2026-06-22 01:22:06 +0300

    fix  commands files

--- burukov.arkadiy/F0/commands.cpp
+++ burukov.arkadiy/F0/commands.cpp
@@ -1,17 +1,22 @@
 #include "commands.hpp"
 #include <stdexcept>
 
-std::string burukov::readToken(std::istream &in)
+std::string burukov::readToken(std::istream& in)
 {
   char c = '\0';
-  while (in.get(c)) {
-    if (c == ' ' || c == '\t' || c == '\n' || c == '\r') {
+  while (in.get(c))
+  {
+    if (c == ' ' || c == '\t' || c == '\n' || c == '\r')
+    {
       continue;
     }
-    if (c == '"') {
+    if (c == '"')
+    {
       std::string result;
-      while (in.get(c)) {
-        if (c == '"') {
+      while (in.get(c))
+      {
+        if (c == '"')
+        {
           return result;
         }
         result += c;
@@ -20,8 +25,10 @@ std::string burukov::readToken(std::istream &in)
     }
     std::string result;
     result += c;
-    while (in.get(c)) {
-      if (c == ' ' || c == '\t' || c == '\n' || c == '\r') {
+    while (in.get(c))
+    {
+      if (c == ' ' || c == '\t' || c == '\n' || c == '\r')
+      {
         return result;
       }
       result += c;
@@ -31,13 +38,14 @@ std::string burukov::readToken(std::istream &in)
   throw std::runtime_error("unexpected eof");
 }
 
-void burukov::parsingAddTitle(std::istream &in, std::ostream &out,
-  LibraryManager &lib)
+void burukov::parsingAddTitle(std::istream& in, std::ostream& out,
+  LibraryManager& lib)
 {
   std::string title = readToken(in);
   std::string author = readToken(in);
   int year = 0;
-  if (!(in >> year)) {
+  if (!(in >> year))
+  {
     throw std::runtime_error("invalid year");
   }
   std::string genre = readToken(in);
@@ -45,8 +53,8 @@ void burukov::parsingAddTitle(std::istream &in, std::ostream &out,
   out << "<ADDED>\n";
 }
 
-void burukov::parsingAddCopy(std::istream &in, std::ostream &out,
-  LibraryManager &lib)
+void burukov::parsingAddCopy(std::istream& in, std::ostream& out,
+  LibraryManager& lib)
 {
   std::string title = readToken(in);
   std::string copyId = readToken(in);
@@ -54,8 +62,8 @@ void burukov::parsingAddCopy(std::istream &in, std::ostream &out,
   out << "<OK>\n";
 }
 
-void burukov::parsingLend(std::istream &in, std::ostream &out,
-  LibraryManager &lib)
+void burukov::parsingLend(std::istream& in, std::ostream& out,
+  LibraryManager& lib)
 {
   std::string title = readToken(in);
   std::string reader = readToken(in);
@@ -63,85 +71,92 @@ void burukov::parsingLend(std::istream &in, std::ostream &out,
   out << "<ISSUED>\n";
 }
 
-void burukov::parsingReturn(std::istream &in, std::ostream &out,
-  LibraryManager &lib)
+void burukov::parsingReturn(std::istream& in, std::ostream& out,
+  LibraryManager& lib)
 {
   std::string copyId = readToken(in);
   lib.returnCopy(copyId);
   out << "<RETURNED>\n";
 }
 
-void burukov::parsingDemandTitle(std::istream &in, std::ostream &out,
-  LibraryManager &lib)
+void burukov::parsingDemandTitle(std::istream& in, std::ostream& out,
+  LibraryManager& lib)
 {
   std::string title = readToken(in);
   int period = 0;
-  if (!(in >> period)) {
+  if (!(in >> period))
+  {
     throw std::runtime_error("invalid period");
   }
   lib.demandModelTitle(out, title, period);
 }
 
-void burukov::parsingDemandGenre(std::istream &in, std::ostream &out,
-  LibraryManager &lib)
+void burukov::parsingDemandGenre(std::istream& in, std::ostream& out,
+  LibraryManager& lib)
 {
   std::string genre = readToken(in);
   int period = 0;
-  if (!(in >> period)) {
+  if (!(in >> period))
+  {
     throw std::runtime_error("invalid period");
   }
   lib.demandModelGenre(out, genre, period);
 }
 
-void burukov::parsingSliceTitle(std::istream &in, std::ostream &out,
-  LibraryManager &lib)
+void burukov::parsingSliceTitle(std::istream& in, std::ostream& out,
+  LibraryManager& lib)
 {
   std::string title = readToken(in);
   int period = 0;
-  if (!(in >> period)) {
+  if (!(in >> period))
+  {
     throw std::runtime_error("invalid period");
   }
   lib.sliceTitle(out, title, period);
 }
 
-void burukov::parsingSliceGenre(std::istream &in, std::ostream &out,
-  LibraryManager &lib)
+void burukov::parsingSliceGenre(std::istream& in, std::ostream& out,
+  LibraryManager& lib)
 {
   std::string genre = readToken(in);
   int period = 0;
-  if (!(in >> period)) {
+  if (!(in >> period))
+  {
     throw std::runtime_error("invalid period");
   }
   lib.sliceGenre(out, genre, period);
 }
 
-void burukov::parsingRecommend(std::istream &in, std::ostream &out,
-  LibraryManager &lib)
+void burukov::parsingRecommend(std::istream& in, std::ostream& out,
+  LibraryManager& lib)
 {
   std::string title = readToken(in);
   size_t k = 0;
-  if (!(in >> k)) {
+  if (!(in >> k))
+  {
     throw std::runtime_error("invalid k");
   }
   lib.recommend(out, title, k);
 }
 
-void burukov::parsingDeadStock(std::istream &in, std::ostream &out,
-  LibraryManager &lib)
+void burukov::parsingDeadStock(std::istream& in, std::ostream& out,
+  LibraryManager& lib)
 {
   int period = 0;
   double threshold = 0.0;
-  if (!(in >> period >> threshold)) {
+  if (!(in >> period >> threshold))
+  {
     throw std::runtime_error("invalid input");
   }
   lib.deadStock(out, period, threshold);
 }
 
-void burukov::parsingDemandBalance(std::istream &in, std::ostream &out,
-  LibraryManager &lib)
+void burukov::parsingDemandBalance(std::istream& in, std::ostream& out,
+  LibraryManager& lib)
 {
   int period = 0;
-  if (!(in >> period)) {
+  if (!(in >> period))
+  {
     throw std::runtime_error("invalid period");
   }
   lib.demandBalance(out, period);
